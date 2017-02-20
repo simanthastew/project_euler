5fc5615552cfc267857a0cbba82a55426451e4f3
[01;31m[K"name"[m[K: "Samantha Stewart <[01;31m[K"email"[m[K: null>
2017-02-20 13:16:36 -0600

    problem 3

---
+++ largest_prime_factor.rb
@@ -0,0 +1,24 @@
+require 'prime'
+
+def factors(num)
+	factors = []
+	for x in (2..num - 1)
+		factors << x if num % x == 0
+	end
+	factors
+end
+
+def is_prime(num)
+	prime_nums = []
+	nums_to_check = factors(num)
+	nums_to_check.each do |x| 
+		prime_nums << x if Prime.prime?(x)
+	end
+	prime_nums
+end
+
+def largest_prime_factor(num)
+	is_prime?(num).sort.last
+end
+
+largest_prime_factor(600851475143)
